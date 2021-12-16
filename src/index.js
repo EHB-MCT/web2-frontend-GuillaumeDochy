c28e6133a01d18d70fe7496121a937752f73927a
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-12-16 18:28:16 +0100

    Added features
    Clicking on a game will now show all the information and screenshots. Clicking on a genre redirects to a certain page, but loading games from this genre does not work yet. Installed packages to make the screenshots appearance better if time permits it. Also changed the appearance of the games on the homepage and the genre page

--- src/index.js
+++ src/index.js
@@ -83,6 +83,38 @@ window.onload = function () {
 
     callHomePage()
 
+    async function genresCall() {
+        setTimeout(500)
+
+        const genres = document.getElementById('genres')
+
+        let response3 = await fetch(`https://api.rawg.io/api/genres?key=879cb43fa6024d69b614737f14e041f6`)
+
+        if (response3.ok) { // HTTP-status = 200-299
+            let data3 = await response3.json();
+
+            let fullGenresList = data3.results
+
+            let htmlString2 = ''
+
+            console.log(fullGenresList)
+
+            fullGenresList.forEach(e => {
+                htmlString2 += `
+                <div class="cards genres clicky searchCards" id="${e.id}">
+                    <img src="${e.image_background}">
+                    <h3 class="gameName">${e.name}</h3>
+                </div>
+                `
+            })
+            genres.innerHTML = htmlString2
+        } else {
+            alert("HTTP-Error: " + response3.status);
+        }
+    }
+
+    genresCall()
+
     function clicked() {
         const gameClicked = document.getElementsByClassName("cards")
         let games = [].slice.call(gameClicked)
@@ -101,8 +133,10 @@ window.onload = function () {
                 console.log(clik.id)
 
                 let id = clik.id
+                let name = clik.name
 
                 sessionStorage.setItem("id", id)
+                sessionStorage.setItem("name", name)
 
                 window.location.href = "./game.html"
 
@@ -131,35 +165,38 @@ window.onload = function () {
                 let bg = data.background_image
                 let name = data.name
                 let release = data.released
-                let rating = data.rating
-                let ratingTop = data.rating_top
+                let rating = data.metacritic
+                let slug = data.slug
+                let description = data.description
 
                 let platform = [];
 
                 data.platforms.forEach(e => {
-                    platform.push(` ${e.platform.name}`);
-                });
+                    platform.push(` ${e.platform.name}`)
+                })
 
-                let genre = [];
+                let genre = []
+                let genreArray = data.genres
 
-                data.genres.forEach(e => {
-                    platform.push(` ${e.name}`);
-                });
+                genreArray.forEach(e => {
+                    genre.push(` ${e.name}`)
+                })
 
                 let publisher = [];
 
                 data.publishers.forEach(e => {
-                    publisher.push(` ${e.name}`);
-                });
+                    publisher.push(` ${e.name}`)
+                })
 
-                let developer = [];
+                let developer = []
 
                 data.developers.forEach(e => {
-                    developer.push(` ${e.name}`);
-                });
+                    developer.push(` ${e.name}`)
+                })
 
                 console.log(platform)
                 console.log(publisher)
+                console.log(genre)
 
                 let htmlString = `
                 <div class="gameSpecific">
@@ -189,12 +226,36 @@ window.onload = function () {
                 </div>
                 <div class="specificDiv">
                 <h4>Ratings:</h4>
-                <p>${rating}/${ratingTop}</p>
+                <p>${rating}</p>
+                </div>
+                <div id="rest">
+                <div id="description">
+                <h4>Description:</h4>
+                <p>${description}</p>
+                </div>
+                <div id="screens">
+                </div>
                 </div>
                 </div>
                 `
                 // container.insertAdjacentHTML('beforeend', htmlString)
                 container.innerHTML = htmlString
+
+                let response2 = await fetch(`https://api.rawg.io/api/games/${slug}/screenshots?key=879cb43fa6024d69b614737f14e041f6`);
+
+                let data2 = []
+                data2 = await response2.json();
+                console.log(data2)
+
+                let htmlString2 = ""
+
+                data2.results.forEach(e => {
+                    htmlString2 += `
+                        <img src="${e.image}">`
+                })
+
+                const container2 = document.getElementById("screens")
+                container2.innerHTML = htmlString2
             } else {
                 alert("HTTP-Error: " + response.status);
             }
@@ -206,9 +267,58 @@ window.onload = function () {
             clik.addEventListener("click", function (e) {
                 e.preventDefault()
                 console.log('clicked')
+
+                let nameGenre = clik.slug
+
+                console.log(nameGenre)
+
+                sessionStorage.setItem("nameGenre", nameGenre)
+                getGenre()
+
                 window.location.href = "./genre.html"
             })
         })
+
+        async function getGenre() {
+            setTimeout(500)
+
+            let genre = sessionStorage.getItem("nameGenre")
+
+            let response = await fetch(`https://api.rawg.io/api/games?genres=${genre}&key=879cb43fa6024d69b614737f14e041f6`);
+
+            if (response.ok) { // HTTP-status = 200-299
+                let data = await response.json();
+
+                let dataResults = data.results
+                // let data = response.results
+                console.log(dataResults)
+
+                let htmlString = ''
+
+                console.log(dataResults)
+
+                dataResults.forEach(e => {
+                    htmlString += `
+                    <a href="./game.html" class="cards clicky" id="${e.id}">
+                        <img src="${e.background_image}" >
+                        <h3 class="gameName">${e.name}</h3>
+                        <div>
+                        <h4>Release date:</h4>
+                        <p class="release">${e.released}</p>
+                        </div>
+                        <div>
+                        <h4>Ratings:</h4>
+                        <p>${e.rating}/${e.rating_top}</p>
+                        </div>
+                    </a>
+                    `
+                })
+
+                document.getElementById('genres').innerHTML = htmlString
+            } else {
+                alert("HTTP-Error: " + response.status);
+            }
+        }
     }
 
     setTimeout(clicked, 1000)
@@ -266,37 +376,8 @@ window.onload = function () {
             } else {
                 alert("HTTP-Error: " + response.status);
             }
-
-            setTimeout(clicked, 1000)
         })
     }
 
-    async function genresCall() {
-        let response2 = await fetch(`https://api.rawg.io/api/genres?key=879cb43fa6024d69b614737f14e041f6`)
-        if (response2.ok) { // HTTP-status = 200-299
-            let data2 = await response2.json();
-
-            let fullGenresList = data2.results
-
-            let htmlString = ''
-
-            console.log(fullGenresList)
-
-            fullGenresList.forEach(e => {
-                htmlString += `
-                <div class="cards genres clicky searchCards" id="${e.id}">
-                    <img src="${e.image_background}">
-                    <h3 class="gameName">${e.name}</h3>
-                </div>
-                `
-                document.getElementById('genres').innerHTML = htmlString
-            })
-        } else {
-            alert("HTTP-Error: " + response2.status);
-        }
-    }
-
     homeSearch()
-
-    setTimeout(genresCall, 1000)
 }
